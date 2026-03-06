eef274b1af207e32da576d62ee5a21d9bc75866b
Ali <ali.razapu@gmail.com>
2026-03-06 12:50:45 +0500

    Update Version

--- application/views/templates/layout/script-files.php
+++ application/views/templates/layout/script-files.php
@@ -1,23 +1,23 @@
 <?php
     $scripts = array(
-        URL::base() . 'plugins/jQuery/jquery-2.2.3.min.js?v=20100200400',
-        URL::base() . 'bootstrap/js/bootstrap.min.js?v=20100200400',
-        URL::base() . 'dist/js/app.min.js?v=20100200400',
-        URL::base() . 'plugins/pace/pace.min.js?v=20100200400',
-        URL::base() . 'plugins/jquery.confirm/jquery.confirm.js?v=20100200400',
-        URL::base() . 'plugins/datatables/jquery.dataTables.min.js?v=20100200400',
-        URL::base() . 'plugins/datatables/dataTables.bootstrap.min.js?v=20100200400',
-        URL::base() . 'dist/js/jquery.validate.js?v=20100200400',
-        URL::base() . 'plugins/datepicker/bootstrap-datepicker.js?v=20100200400',
-        URL::base() . 'plugins/datepicker/bootstrap-datetimepicker.js?v=20100200400',
-        URL::base() . 'dist/js/cytoscape.min.js?v=20100200400',        
-        URL::base() . 'dist/js/date_time.js?v=20100200400', 
+        URL::base() . 'plugins/jQuery/jquery-2.2.3.min.js?v=20100200401',
+        URL::base() . 'bootstrap/js/bootstrap.min.js?v=20100200401',
+        URL::base() . 'dist/js/app.min.js?v=20100200401',
+        URL::base() . 'plugins/pace/pace.min.js?v=20100200401',
+        URL::base() . 'plugins/jquery.confirm/jquery.confirm.js?v=20100200401',
+        URL::base() . 'plugins/datatables/jquery.dataTables.min.js?v=20100200401',
+        URL::base() . 'plugins/datatables/dataTables.bootstrap.min.js?v=20100200401',
+        URL::base() . 'dist/js/jquery.validate.js?v=20100200401',
+        URL::base() . 'plugins/datepicker/bootstrap-datepicker.js?v=20100200401',
+        URL::base() . 'plugins/datepicker/bootstrap-datetimepicker.js?v=20100200401',
+        URL::base() . 'dist/js/cytoscape.min.js?v=20100200401',
+        URL::base() . 'dist/js/date_time.js?v=20100200401',
         //mark js files
-        URL::base() . 'dist/mark/js/datatables.mark.js?v=20100200400', 
-        URL::base() . 'dist/mark/js/jquery.mark.js?v=20100200400', 
+        URL::base() . 'dist/mark/js/datatables.mark.js?v=20100200401',
+        URL::base() . 'dist/mark/js/jquery.mark.js?v=20100200401',
         //end mark js files
         //Datatables responsive js files
-        URL::base() . 'dist/js/dataTables.responsive.js?v=20100200400',         
+        URL::base() . 'dist/js/dataTables.responsive.js?v=20100200401',
         //end Datatables responsive js files
 
        // URL::base() . 'dist/js/bootstrap-datetimepicker.min.js',        
